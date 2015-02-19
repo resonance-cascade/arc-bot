82fd4155b32f3a6a4f4477e61535b9c9e97d23b8
Bret Comnes <bcomnes@gmail.com>
2015-02-19 09:52:09 -0800

    Added karma detector

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-var irc = require('irc');
+var irc = require('irc')
 
 var config = {
-	channels: ['#arc'],
-	server: 'irc.cat.pdx.edu',
-	botName: 'arc-bot'
+  channels: ['#arc'],
+  server: 'irc.cat.pdx.edu',
+  botName: 'arc-bot'
 }
 
-var bot = new irc.Client(config.server, config.botName, { channels: config.channels, debug: true })
+var bot = new irc.Client(config.server, config.botName, {
+  channels: config.channels,
+  debug: true
+})
 
 bot.addListener('join', function(channel, who) {
-	console.log(channel, who)
+  console.log(channel, who)
 })
 
-bot.addListener('message', function(from, to, text, message) {
-	console.log(from, to, text)
+bot.addListener('message#', function(from, to, text, message) {
+  console.log(from, to, text)
+  var karma = karmaGetter(text)
+  if (karma) {
+    karmaSaver(karma[0], function(err, totalKarma) {
+      if (err) {
+        console.log(err)
+      }
+      bot.say(to, [karma[0], 'has', totalKarma, 'karma'].join(' '))
+    })
+  }
 })
 
+bot.addListener('pm', function(from, text, message) {
+  console.log('PM: ', from, text)
+})
+
+function karmaGetter (string) {
+  return string.match(/\w+(?=\+{2})/)
+}
+x
+function karmaSaver (recipient, cb) {
+  cb(null, 'over 5000')
+}
