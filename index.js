e19180b3322500f8302bee401ea48d9e38efeb6c
Bret Comnes <bcomnes@gmail.com>
2015-02-19 10:10:39 -0800

    Added a karmastore

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var irc = require('irc')
+var levelup = require('level')
+
+var db = levelup('./karmadb')
 
 var config = {
   channels: ['#arc'],
@@ -37,5 +40,19 @@ function karmaGetter (string) {
 }
 
 function karmaSaver (recipient, cb) {
-  cb(null, 'over 5000')
+  db.get(recipient, function(err, value) {
+    if (err) {
+      if (err.type === 'NotFoundError') {
+        // Initialize the name
+        db.put(recipient, 1, function(err) {
+          return err ? cb(err) : cb(null, 1)
+        })
+      }
+      return cb(err)
+    }
+    var karma = value++
+    db.put(recipient, karma, function(err) {
+      return err ? cb(err) : cb(null, karma)
+    })
+  })
 }
